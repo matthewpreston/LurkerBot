2396ecbbd7a5ca294f28195f9f72564a0ada6a11
Oliver Dowd <notvincent42@gmail.com>
2017-03-25 16:01:35 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
 
+import time
+import random
+
+
 def scrape(submission):
-	return 6
+	log = open('data.txt','w')
+	t = submission.title
+	desc = submission.selftext
+	comments = submission.comments.list()
+	log.write(t + "\n")
+	log.write(desc + "n")
+	for item in comments:
+		log.write(item.body + "\n")
+	i = random.randint(3,120)
+	print("Lurking... this may take " + str(i) + " seconds")
+	log.close()
+	time.sleep(i)
